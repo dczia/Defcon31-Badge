744b136cce10699a885cd976e719b3dec6e37308
beardbyte <beardbyte@gmail.com>
2023-07-22 11:08:53 -0500

    Add menu selection highlighting

--- Software/Testing/main.py
+++ Software/Testing/main.py
@@ -154,8 +154,9 @@ def show_menu(menu):
     line_height = 10
     offset = 5
 
-    # clear the display
-    #display.fill_rect(0,0,width,height,0)
+    color_bitmap = displayio.Bitmap(width, line_height, 1)
+    color_palette = displayio.Palette(1)
+    color_palette[0] = 0xFFFFFF # White
 
     # Shift the list of files so that it shows on the display
     list_length = len(menu)
@@ -163,17 +164,21 @@ def show_menu(menu):
 
     for item in short_list:
         if highlight == line:
+            white_rectangle = displayio.TileGrid(color_bitmap,
+                               pixel_shader=color_palette,
+                               x=0, y=((line-1)*line_height))
+            display_group.append(white_rectangle)
             text_arrow = '>'
-            text_arrow = label.Label(terminalio.FONT, text=text_arrow, color=0xFFFFFF, x=0, y=((line-1)*line_height)+offset)
+            text_arrow = label.Label(terminalio.FONT, text=text_arrow, color=0x000000, x=0, y=((line-1)*line_height)+offset)
             display_group.append(text_arrow)
-            text_item = label.Label(terminalio.FONT, text=item, color=0xFFFFFF, x=10, y=((line-1)*line_height)+offset)
+            text_item = label.Label(terminalio.FONT, text=item, color=0x000000, x=10, y=((line-1)*line_height)+offset)
             display_group.append(text_item)
             #display.fill_rect(0,(line-1)*line_height, width,line_height,1)
             #display.text(">",0, (line-1)*line_height,0)
             #display.text(item, 10, (line-1)*line_height,0)
             #display.show()
         else:
-            text_item = label.Label(terminalio.FONT, text=item, color=0xFFFFFF, x=10, y=((line-1)*line_height)+offset)
+            text_item = label.Label(terminalio.FONT, text=item, color = 0xFFFFFF, x=10, y=((line-1)*line_height)+offset)
             display_group.append(text_item)
             #display.text(item, 10, (line-1)*line_height,1)
             #display.show()
