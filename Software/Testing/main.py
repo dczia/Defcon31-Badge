d126465e19080139c726362bdbfbd6381c7fc9ce
Mat R <1577341+Ancient123@users.noreply.github.com>
2023-07-27 19:42:46 -0600

    MOAR PRETTY COLORS (#33)

--- Software/Testing/main.py
+++ Software/Testing/main.py
@@ -29,6 +29,9 @@ from adafruit_midi.note_off import NoteOff
 from adafruit_midi.control_change import ControlChange
 from adafruit_led_animation.animation.rainbow import Rainbow
 from adafruit_led_animation.animation.rainbowchase import RainbowChase
+from adafruit_led_animation.animation.rainbowcomet import RainbowComet
+from adafruit_led_animation.animation.rainbowsparkle import RainbowSparkle
+from adafruit_led_animation.animation.sparklepulse import SparklePulse
 from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 from adafruit_hid.keyboard import Keyboard
@@ -735,16 +738,28 @@ class MIDIState(State):
             machine.go_to_state("menu")
 
 class FlashyState(State):
-    rainbow = False
+    animation = False
 
     menu_items = [
+        {
+            "function": "rainbow",
+            "pretty": "Rainbow",
+        },
         {
             "function": "rainbow_chase",
             "pretty": "Rainbow Chase",
         },
         {
-            "function": "rainbow",
-            "pretty": "Rainbow",
+            "function": "rainbow_comet",
+            "pretty": "Rainbow Comet",
+        },
+        {
+            "function": "rainbow_sparkle",
+            "pretty": "Rainbow Sparkle",
+        },
+        {
+            "function": "sparkle_pulse",
+            "pretty": "Sparkle Pulse",
         },
     ]
 
@@ -776,13 +791,19 @@ class FlashyState(State):
             display.show(text_area)
             self.last_position = position
             if self.menu_items[index]["function"] == "rainbow":
-                self.rainbow = Rainbow(neopixels, speed=0.1)
+                self.animation = Rainbow(neopixels, speed=0.1)
             elif self.menu_items[index]["function"] == "rainbow_chase":
-                self.rainbow = RainbowChase(neopixels, speed=0.1)
+                self.animation = RainbowChase(neopixels, speed=0.1)
+            elif self.menu_items[index]["function"] == "rainbow_comet":
+                self.animation = RainbowComet(neopixels, speed=0.1, tail_length=10)
+            elif self.menu_items[index]["function"] == "rainbow_sparkle":
+                self.animation = RainbowSparkle(neopixels, speed=0.1, period=5, num_sparkles=None, step=1)
+            elif self.menu_items[index]["function"] == "sparkle_pulse":
+                self.animation = SparklePulse(neopixels, speed=0.1, color=(0,255,0), period=5, max_intensity=1, min_intensity=0)
         else:
             # All animations need to impliment animate() as their step function
-            if self.rainbow and self.rainbow.animate:
-                self.rainbow.animate()
+            if self.animation and self.animation.animate:
+                self.animation.animate()
 
         enc_buttons_event = enc_buttons.events.get()
         if enc_buttons_event and enc_buttons_event.pressed:
